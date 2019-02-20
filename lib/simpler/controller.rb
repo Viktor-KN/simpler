b894835006db843d745b091eeb491908f318bf1d
Viktor Kachulin <vicart2008@gmail.com>
2019-02-20 23:55:49 +1000

    Add ability to define params in routes

--- lib/simpler/controller.rb
+++ lib/simpler/controller.rb
@@ -9,6 +9,7 @@ module Simpler
       @name = extract_name
       @request = Rack::Request.new(env)
       @response = Rack::Response.new
+      merge_params
     end
 
     def make_response(action)
@@ -26,6 +27,10 @@ module Simpler
 
     def extract_name
       self.class.name.match('(?<name>.+)Controller')[:name].downcase
+    end
+
+    def merge_params
+      @request.params.merge!(@request.env['simpler.route_params'])
     end
 
     def set_default_headers
